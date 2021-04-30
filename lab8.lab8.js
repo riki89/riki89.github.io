19d7b5e4c2322b0a8e3de70f1d0df466edeb15c6
riki89 <saikoudiallo59@esp.sn>
2021-04-29 23:45:22 -0500

    Create lab8.lab8.js

---
+++ lab8.lab8.js
@@ -0,0 +1,125 @@
+student = {
+    firstName: '',
+    lastName: '',
+    grades: [],
+    inputNewGrade (newGrade){
+        this.grades.push(newGrade);
+    },
+    computeAverageGrade(){
+        let result = this.grades.reduce((accumulator, val) => (accumulator  + val), 0);
+        let avg = result/this.grades.length;
+        return avg;
+    },
+}
+
+students = [
+]
+
+let student1 = Object.create(student);
+
+student1.firstName = "Yaya";
+student1.lastName = "Diallo";
+student1.inputNewGrade(3); 
+student1.inputNewGrade(3.2); 
+student1.inputNewGrade(3.4); 
+student1.inputNewGrade(3.6);
+
+console.log(student1.computeAverageGrade());
+
+let student2 = Object.create(student);
+
+student2.firstName = "Ahmed";
+student2.lastName = "Snow";
+student2.inputNewGrade(3); 
+student2.inputNewGrade(3.2); 
+student2.inputNewGrade(3.4); 
+student2.inputNewGrade(4);
+
+console.log(student2.computeAverageGrade());
+students.push(student2)
+
+let result = students.map(a => a.computeAverageGrade()).reduce((accumulator, val) => (accumulator  + val), 0);
+        let avg = result/students.length;
+console.log(avg);
+
+//
+function Student(firstName, lastName, grades){
+    this.inputNewGrade = function(newGrade){
+        grades.push(newGrade);
+    };
+    this.computeAverageGrade = function(){
+        let result = grades.reduce((accumulator, val) => (accumulator  + val), 0);
+        let avg = result/grades.length;
+        return avg;
+    };
+}
+
+
+let stud1 = new Student("John", "Snow", [3, 3.2, 4]);
+let stud2 = new Student("James", "Rodrigues", [3.2, 3.3, 4]);
+studs = [
+    stud1,
+    stud2
+];
+
+let result = studs.map(a => a.computeAverageGrade()).reduce((accumulator, val) => (accumulator  + val), 0);
+        let avg = result/studs.length;
+console.log(avg);
+
+stud1.constructor.sort();
+
+//
+Array.prototype.mySort = function() { 
+    return this.sort((a,b)=>{
+    return a.lastName > b.lastName ? 1:-1
+    });
+    
+   }
+    
+   console.log(studs.mySort());
+
+
+//Use object literal and constructor function to implement a single LinkedList.
+//Object litteral
+linkedlist = {
+    values: [],
+    add: function(a){
+        this.values.push(a);
+    },
+    remove: function(a){
+        this.values.pop(a);
+    },
+    print: function(){
+        console.log(this.values);
+    }
+}
+ 
+linkedlist.add(1);
+linkedlist.add(2);
+linkedlist.add(3);
+linkedlist.print(); //Expected Result: LinkedList{1,2,3};
+linkedlist.remove(2);
+linkedlist.print(); //Expected Result: LinkedList{1,3};
+//
+//using constructor function
+function LinkedList(){
+    this.values = [];
+    this.add = function(a){
+        this.values.push(a);
+    }
+    this.remove = function(a){
+        this.values.pop(a);
+    } 
+    this.print = function(){
+        console.log(this.values);
+    }
+}
+let linkedlist = new LinkedList();
+linkedlist.add(1);
+linkedlist.add(2);
+linkedlist.add(3);
+linkedlist.print(); //Expected Result: LinkedList{1,2,3};
+linkedlist.remove(2);
+linkedlist.print(); //Expected Result: LinkedList{1,3};
+//
+
